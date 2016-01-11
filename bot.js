87bd235cc993e67b473f69c76b7c6def6b6b73bf
dawsome <dawsome@users.noreply.github.com>
2016-01-11 00:51:12 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,14 +13,7 @@ function respond() {
   } 
   else if(request.text && botRegexHa.test(request.text.substring((request.text.length-10),request.text.length))) {
     this.res.writeHead(200);
-    getit = Math.floor(Math.random()*11);
-    postMessage(getit);
-    if(getit = 7) {
-      postMessage("Tbh I didn't really get that one.");
-    }
-    else {
-      postMessage("Yes Christian you are very funny ahaha");
-    }
+    postMessage("Yes Christian you are very funny ahaha");
     this.res.end();
   }
   
